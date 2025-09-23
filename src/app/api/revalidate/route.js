3add3112443e34435bc0fc92f2dd54c745e0230a
Hassan <faydnoor@gmail.com>
2025-09-23 20:14:51 +0300

    revaldate

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -2,6 +2,8 @@
 import { revalidatePath } from 'next/cache'
 import { NextResponse } from 'next/server'
 
+export const runtime = 'edge';
+
 export async function POST(request) {
   // اقرأ المفتاح السري من الـ Headers
   const secret = request.headers.get('x-revalidate-secret')
